481f61ecb57635bbd549efb7c611cf13e929148b
toiyabe <52581997+toiyabe@users.noreply.github.com>
2022-08-06 10:18:25 -0500

    update timer for top of the hour

--- Mars.CreateDemoDatabase/BuildDemoDb.cs
+++ Mars.CreateDemoDatabase/BuildDemoDb.cs
@@ -22,7 +22,7 @@ namespace Mars.CreateDemoDatabase
         }
 
         [FunctionName("BuildDemoDb")]
-        public async Task Run([TimerTrigger("0 0 */4 * * *")] TimerInfo myTimer, ILogger log)
+        public async Task Run([TimerTrigger("0 0 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
